ca83751b39d4383d04bee673e712858d5ad73993
JakubKub13 <jakubkubala3@gmail.com>
2022-11-12 13:15:30 +0100

    Setting boilerplate for services

--- src/app/m-dai.service.ts
+++ src/app/m-dai.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@angular/core';
 import { ethers, BigNumber } from 'ethers';
 import bigNumberToETHString from 'src/helpers/bigNumberToETHString';
 import * as AddressesJSON from '../assets/contractAddresses.json';
-import * as MockDaiJSON from '../assets/mDAI.json';
+import * as AuctionFactoryJSON from '../assets/AuctionFactory.json';
+import * as AuctionImplementationJSON from '../assets/AuctionImplementation.json';
+import * as mockDaiJSON from '../assets/mDAI.json';
+import * as NftJSON from '../assets/nft.json';
+import currentEpoch from 'src/helpers/currentEpoch';
 
 @Injectable({
   providedIn: 'root'
